8a0b540c1dbcd716e8fed507090c0cb42ead7c8a
K.G. O'Hearon <ohearonkg@gmail.com>
2018-10-07 00:03:31 -0300

    circular references handled with javascript closures.

--- schema.js
+++ schema.js
@@ -2,33 +2,48 @@ var GraphQLObjectType = require("graphql").GraphQLObjectType;
 var GraphQLString = require("graphql").GraphQLString;
 var GraphQLInt = require("graphql").GraphQLInt;
 var GraphQLSchema = require("graphql").GraphQLSchema;
+var GraphQLList = require("graphql").GraphQLList;
 var axios = require("axios");
 
 var CompanyType = new GraphQLObjectType({
   name: "Company",
-  fields: {
-    id: { type: GraphQLString },
-    name: { type: GraphQLString },
-    description: { type: GraphQLString }
+  fields: function() {
+    return {
+      id: { type: GraphQLString },
+      name: { type: GraphQLString },
+      description: { type: GraphQLString },
+      users: {
+        type: GraphQLList(UserType),
+        resolve: function(parentValue, args) {
+          return axios
+            .get("http://localhost:3000/companies/" + parentValue.id + "/users")
+            .then(function(res) {
+              return res.data;
+            });
+        }
+      }
+    };
   }
 });
 
 var UserType = new GraphQLObjectType({
   name: "User",
-  fields: {
-    id: { type: GraphQLString },
-    firstName: { type: GraphQLString },
-    age: { type: GraphQLInt },
-    company: {
-      type: CompanyType,
-      resolve: function(parentValue, args) {
-        return axios
-          .get("http://localhost:3000/companies/" + parentValue.companyId)
-          .then(function(res) {
-            return res.data;
-          });
+  fields: function() {
+    return {
+      id: { type: GraphQLString },
+      firstName: { type: GraphQLString },
+      age: { type: GraphQLInt },
+      company: {
+        type: CompanyType,
+        resolve: function(parentValue, args) {
+          return axios
+            .get("http://localhost:3000/companies/" + parentValue.companyId)
+            .then(function(res) {
+              return res.data;
+            });
+        }
       }
-    }
+    };
   }
 });
 
@@ -41,6 +56,17 @@ var RootQuery = new GraphQLObjectType({
       resolve: function(parentValue, args) {
         return axios
           .get("http://localhost:3000/users/" + args.id)
+          .then(function(res) {
+            return res.data;
+          });
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve: function(parentValue, args) {
+        return axios
+          .get("http://localhost:3000/companies/" + args.id)
           .then(function(res) {
             return res.data;
           });
